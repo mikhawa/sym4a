611bdf4b92f0ad24b1b05181d5dc0d12fc32ee63
Mikhawa <michaeljpitz@gmail.com>
2018-02-21 11:02:20 +0000

    twig extends Controller and templates

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,10 +2,11 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class LuckyController
+class LuckyController extends Controller
 {
     /**
      * 
@@ -15,8 +16,8 @@ class LuckyController
     {
         $number = mt_rand(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
     }
 }
