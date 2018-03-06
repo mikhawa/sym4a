3fb809b631c308c6bb1ba626007852d48058f5b3
Mikhawa <michaeljpitz@gmail.com>
2018-03-06 12:35:42 +0000

    composer require --dev phpunit

---
+++ tests/Entity/ProductTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Tests\Entity;
+
+/**
+ * Description of ProductTest
+ *
+ * @author Michael
+ */
+class ProductTest {
+    //put your code here
+}
